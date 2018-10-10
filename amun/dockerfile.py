81ee16b4cc7b38bf933ea30b2b1096eeec98827f
Fridolin Pokorny <fridolin@redhat.com>
2018-10-10 20:59:18 +0200

    Make sure to expand new lines correctly

--- amun/dockerfile.py
+++ amun/dockerfile.py
@@ -63,9 +63,9 @@ def _write_file_string(content: str, path: str) -> str:
     # TODO: accept a list of files so we generate only one layer for all files
     # TODO: escape content
     # TODO: handle it in nice way so we can see it nicely in OpenShift's configuration
-    content = content.replace('"', '\\"').replace('\n', '\\\n')
+    content = content.replace('"', '\\"').replace('\n', '\\n\\\n')
     path = path.replace('"', '\"')
-    return f'RUN echo "{content}" >"{path}"\n'
+    return f'RUN echo -e "{content}" >"{path}"\n'
 
 
 def create_dockerfile(specification: dict) -> tuple:
