9d3a0e4ba2bcfa289611000a61066e26374986f2
Fridolin Pokorny <fridolin@redhat.com>
2019-09-23 13:53:34 +0200

    Make Amun API OpenAPI v3 compliant

--- amun/configuration.py
+++ amun/configuration.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Amun
+# thoth-user-api
 # Copyright(C) 2018, 2019 Fridolin Pokorny
 #
 # This program is free software: you can redistribute it and / or modify
@@ -17,13 +17,41 @@
 
 """Configuration of Amun API service."""
 
+import logging
 import os
+
+from jaeger_client import Config as JaegerConfig
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class Configuration:
     """Configuration of Amun API service."""
 
     APP_SECRET_KEY = os.environ['AMUN_API_APP_SECRET_KEY']
-    SWAGGER_YAML_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'swagger.yaml')
+    SWAGGER_YAML_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../openapi")
     AMUN_INSPECTION_NAMESPACE = os.environ['THOTH_AMUN_INSPECTION_NAMESPACE']
     AMUN_INFRA_NAMESPACE = os.environ['THOTH_AMUN_INFRA_NAMESPACE']
+
+    JAEGER_HOST = os.getenv("JAEGER_HOST", "localhost")
+
+    OPENAPI_PORT = 8080
+
+    tracer = None
+
+
+def init_jaeger_tracer(service_name):
+    """Create a Jaeger/OpenTracing configuration."""
+    config = JaegerConfig(
+        config={
+            "sampler": {"type": "const", "param": 1},
+            "logging": True,
+            "local_agent": {"reporting_host": Configuration.JAEGER_HOST},
+        },
+        service_name=service_name,
+        validate=True,
+        metrics_factory=PrometheusMetricsFactory(namespace=service_name),
+    )
+
+    return config.initialize_tracer()
