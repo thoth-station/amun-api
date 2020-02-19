3a9c362aa27e9fed3b035fa233db569f61c025f8
Marek Cermak <macermak@redhat.com>
2020-02-19 14:06:28 +0100

    Formatted dockerfile.py
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   amun/dockerfile.py

--- amun/dockerfile.py
+++ amun/dockerfile.py
@@ -32,7 +32,7 @@ from .exceptions import ScriptObtainingError
 _LOGGER = logging.getLogger(__name__)
 
 _HERE = os.path.dirname(os.path.abspath(__file__))
-_ENTRYPOINT_PY = os.path.join(_HERE, 'inspect.py')
+_ENTRYPOINT_PY = os.path.join(_HERE, "inspect.py")
 # Make sure pip and Pipenv trust the AICoE index.
 _PIP_CONF = """
 [global]
@@ -58,7 +58,7 @@ def _determine_installer_string() -> str:
 
 def _obtain_script(script: str) -> str:
     """Obtain script if it was specified by an URL, if script was provided inline, return it."""
-    if script.startswith(('https://', 'http://')):
+    if script.startswith(("https://", "http://")):
         # Download script from remote if needed.
         response = requests.get(script)
         try:
@@ -78,19 +78,19 @@ def _write_file_string(content: str, path: str) -> str:
     # TODO: accept a list of files so we generate only one layer for all files
     # TODO: escape content
     # TODO: handle it in nice way so we can see it nicely in OpenShift's configuration
-    content = content.replace('"', '\\"').replace('\n', '\\n\\\\n')
-    path = path.replace('"', '\"')
+    content = content.replace('"', '\\"').replace("\n", "\\n\\\\n")
+    path = path.replace('"', '"')
     return f'RUN echo -e "{content}" > "{path}"\n\n'
 
 
 def create_dockerfile(specification: dict) -> tuple:
     """Create a Dockerfile based on software stack specification."""
     script_present = False
-    dockerfile = "FROM " + specification['base'] + "\n\n"
+    dockerfile = "FROM " + specification["base"] + "\n\n"
 
     dockerfile += "USER root\n\n"
 
-    env_str = ''
+    env_str = ""
     for environ in specification.get("environment", []):
         env_str += f"{environ['name']}={environ['value']} "
 
@@ -98,62 +98,69 @@ def create_dockerfile(specification: dict) -> tuple:
         dockerfile += f"ENV {env_str}\n\n"
 
     # Updating the base has to be turned on explicitly.
-    if specification.get('update', False):
+    if specification.get("update", False):
         dockerfile += _determine_update_string() + "\n\n"
 
-    if specification.get('packages'):
-        dockerfile += _determine_installer_string() + \
-            " ".join(specification['packages']) + '\n\n'
+    if specification.get("packages"):
+        dockerfile += (
+            _determine_installer_string() + " ".join(specification["packages"]) + "\n\n"
+        )
 
-    if specification.get('python_packages'):
-        dockerfile += "RUN pip3 install --force-reinstall --upgrade " + \
-            " ".join(specification["python_packages"]) + '\n\n'
+    if specification.get("python_packages"):
+        dockerfile += (
+            "RUN pip3 install --force-reinstall --upgrade "
+            + " ".join(specification["python_packages"])
+            + "\n\n"
+        )
 
-    for file_spec in specification.get('files', []):
-        path = file_spec['path']
-        content = file_spec['content']
+    for file_spec in specification.get("files", []):
+        path = file_spec["path"]
+        content = file_spec["content"]
         # This trick helps so that env variables are not expanded.
         dockerfile += _write_file_string(content, path)
 
     # Create workdir only if needed.
-    if 'python' in specification or 'script' in specification:
-        dockerfile += f'RUN mkdir -p /home/amun && chmod -R 777 /home/amun\n\n'
+    if "python" in specification or "script" in specification:
+        dockerfile += f"RUN mkdir -p /home/amun && chmod -R 777 /home/amun\n\n"
 
-    if 'python' in specification:
-        requirements = specification['python']['requirements']
-        requirements_locked = specification['python']['requirements_locked']
+    if "python" in specification:
+        requirements = specification["python"]["requirements"]
+        requirements_locked = specification["python"]["requirements_locked"]
 
         if not any([requirements, requirements_locked]):
             _LOGGER.debug("No requirements specified.")
         elif not all([requirements, requirements_locked]):
             raise ValueError(
-                "Both `requirements` and `requirements_locked` must be provided.")
+                "Both `requirements` and `requirements_locked` must be provided."
+            )
         else:
             pipfile_content = toml.dumps(requirements)
-            dockerfile += _write_file_string(
-                pipfile_content, '/home/amun/Pipfile')
+            dockerfile += _write_file_string(pipfile_content, "/home/amun/Pipfile")
 
             pipfile_lock_content = json.dumps(
-                requirements_locked, sort_keys=True, indent=4)
+                requirements_locked, sort_keys=True, indent=4
+            )
             dockerfile += _write_file_string(
-                pipfile_lock_content, '/home/amun/Pipfile.lock')
-
-            dockerfile += _write_file_string(_PIP_CONF, "/etc/pip.conf")
-            dockerfile += 'RUN cd /home/amun && pipenv install --deploy\n\n'
-
-    if 'script' in specification:
-        script_present = True
-        content = _obtain_script(specification['script'])
-        dockerfile += _write_file_string(content, '/home/amun/script')
-        with open(_ENTRYPOINT_PY, 'r') as entrypoint_file:
-            dockerfile += _write_file_string(
-                entrypoint_file.read(),
-                '/home/amun/entrypoint'
+                pipfile_lock_content, "/home/amun/Pipfile.lock"
             )
 
-        dockerfile += 'RUN chmod a+x /home/amun/script /home/amun/entrypoint && ' \
-            'touch /home/amun/script.stderr /home/amun/script.stdout && ' \
-            'chmod 777 /home/amun/script.stderr /home/amun/script.stdout\n\n'
+            dockerfile += _write_file_string(_PIP_CONF, "/etc/pip.conf")
+            dockerfile += "RUN cd /home/amun && pipenv install --deploy\n\n"
+
+    if "script" in specification:
+        script_present = True
+        content = _obtain_script(specification["script"])
+        dockerfile += _write_file_string(content, "/home/amun/script")
+        with open(_ENTRYPOINT_PY, "r") as entrypoint_file:
+            dockerfile += _write_file_string(
+                entrypoint_file.read(), "/home/amun/entrypoint"
+            )
+
+        dockerfile += (
+            "RUN chmod a+x /home/amun/script /home/amun/entrypoint && "
+            "touch /home/amun/script.stderr /home/amun/script.stdout && "
+            "chmod 777 /home/amun/script.stderr /home/amun/script.stdout\n\n"
+        )
         dockerfile += 'CMD ["/home/amun/entrypoint"]\n\n'
 
     # An arbitrary user.
